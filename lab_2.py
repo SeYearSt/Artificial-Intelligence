4d5578c6387af355e4b2c0899093432bcb80d403
justOleh <olehvenhryniu@gmail.com>
2018-05-12 17:21:36 +0300

    finished

--- lab_2.py
+++ lab_2.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import random
 
-N = 2000
+N = 1000
 
 maxX = 2*np.pi
 phi = random.random() / 10
@@ -34,23 +34,19 @@ def mean_arithmetic(sequence):
 def mean_geometric(sequence):
     sum = 1
     result = []
-    i = 0
-    for x in sequence:
-        sum *= x
-        result.append(np.power(np.abs(sum),(1/(i+1))))
-        i += 1
+    for i in range(len(sequence)):
+        sum *= sequence[i]
+        result.append(np.power(np.abs(sum),1/(i+1)))
     return result
 
 def mean_harmonic(sequence):
-    sum = 0;
-    result_arr = [];
-    i = 0
-    for x in sequence:
-        if np.abs(x) >= 0.05:
-            sum += np.abs(1 / x)
-            result_arr.append((i + 1) / sum)
-        i+=1
-    return result_arr;
+    sum = 0
+    result = []
+    for i in range(len(sequence)):
+        if np.abs(sequence[i]) >= 0.05:
+            sum += np.abs(1 / sequence[i])
+            result.append((i + 1) / sum)
+    return result;
 
 def simple_moving_average(sequence, bars):
         result = []
@@ -60,14 +56,6 @@ def simple_moving_average(sequence, bars):
                 sum += sequence[i + j]
             result.append(sum / bars)
         return result
-        # for j in range(bars):
-        #     result.append(0)
-        # for i in range(bars-1,len(sequence)):
-        #     sum = 0
-        #     for j in range(bars):
-        #         sum += sequence[i - j]
-        #     result.append(sum / bars)
-        # return result
 
 def weighted_moving_averange(sequence, bars):
     result = []
@@ -112,19 +100,10 @@ def smoothing_out(sequence, smoothing):
 
         result.append(sum / ((smoothing*2) + 1))
 
-    # for (i = 0; i < func.length; i++) {
-    #     var sum = 0;
-    #
-    #     for (index = i - smoothing; index <= i + smoothing; index++) {
-    #         var thisIndex = index < 0 ? index + func.length : index % func.length;
-    #         sum += func[thisIndex];
-    #     }
-    #     newArray[i] = sum/((smoothing*2)+1);
-    # }
-
 
 
 sequence = init_sequence()
+
 
 plt.plot(sin_sequence(), label = "sin")
 plt.plot(init_sequence(), label = "changed sin")
@@ -136,6 +115,5 @@ plt.plot(weighted_moving_averange(sequence,9), label = "weighted moving averange
 plt.plot(modified_moving_averange(sequence), label = "modifined moving averange")
 plt.plot(exponential_moving_average(sequence,9), label = "exponential moving average")
 
-# plt.plot(smoothing_out(sequence,20), label = "smoothing out")
 plt.legend()
 plt.show()
