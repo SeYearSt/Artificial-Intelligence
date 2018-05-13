5b6fc303bcf76168ed4ca7de71890a00f85bd926
justOleh <olehvenhryniu@gmail.com>
2018-05-13 14:26:07 +0300

    finished

--- lab_4.py
+++ lab_4.py
@@ -1,64 +1,84 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import random
-import math
 
-
-MIN_X = 0
-MAX_X = math.pi #2*math.pi
-INTERVAL = 100
+'''MINX = 0
+MAXX = 3 #2*math.pi
+'''
 
 N = 300
 
+
 def f(x):
-    #return x
-    return math.sin(x)
+    return np.exp(x)**2
+
+def def_integral(f,MINX, MAXX, seg = 300):
+    S = 0
+
+    X = np.linspace(MINX, MAXX, seg)
+
+    for i in range(len(X) - 1):
+        S += f((X[i] + X[i+1])/2)*(X[i+1] - X[i])
+    return S
 
 
-def create_points(count = N):
-    x_points = []
-    y_points = []
-    for i in range(count):
-        x_points.append(random.uniform(MIN_X, MAX_X*INTERVAL))
-        y_points.append(random.uniform(0, 1))
-    return (x_points, y_points)
+def integral_montecarlo(f, MINX, MAXX):
+
+    def get_min_and_max(f,X):
+        min = max = f(X[0])
+        for x in X:
+            if f(x) > max:
+                max = f(x)
+            elif f(x) < min:
+                min = f(x)
+        return (min, max)
 
 
-def count_points_under_line(f, x_points, y_points):
-    points_count_under_line = 0
-    points_count_out_line = 0
-    for i in range(N):
-        if y_points[i] <= f(x_points[i]):
-            points_count_under_line += 1
-        else:
-            points_count_out_line += 1
-    return (points_count_under_line, points_count_out_line)
+    def create_random_points(min, max, count = N):
+        x_points = []
+        y_points = []
+        x_points.append(np.random.uniform(MINX, MAXX, count))
+        y_points.append(np.random.uniform(min, max, count))
+        return (x_points, y_points)
 
 
-def get_min_and_max(f,X):
-    min = max = X[0]
-    for x in X:
-        if f(x) > max:
-            max = f(x)
-        elif f(x) < min:
-            min = f(x)
-    return (min, max)
+    def count_points_under_chart(f, x_points, y_points, count = N):
+        points_count_under_chart = 0
+        points_count_out_chart = 0
+        for i in range(count):
+            if y_points[0][i] <= f(x_points[0][i]):
+                points_count_under_chart += 1
+        return points_count_under_chart
 
 
-X = [x/INTERVAL for x in range(MIN_X, int(MAX_X * INTERVAL))]
-Y = [f(x) for x in X]
+    X = np.linspace(MINX, MAXX, N)
 
-x_points, y_points = create_points(N)
+    min, max = get_min_and_max(f,X)
 
-K, Z = count_points_under_line(f,x_points, y_points)
+    points_x, points_y = create_random_points(0, max, N)
 
-#print(MAX_X - MIN_X)
-#print(K)
-#print(Z)
-print(get_min_and_max(f, X))
-print("Area of figure", K/N*(MAX_X - MIN_X))      
+    #print(points_x)
+    #print(points_y)
+
+    K = count_points_under_chart(f, points_x, points_y, N)
+
+    print("Points under figure:", K)
+    print("All points points:", N)
+
+    print("Square rectangle:", np.abs(max - min)*np.abs(MAXX - MINX))
+    print("Square figure:", K/N * np.abs(max - min)*np.abs(MAXX - MINX))
 
 
-plt.plot(Y)
-plt.plot()
-plt.scatter(x_points, y_points, color = 'lightblue')
-plt.show()
+
+    plt.axhline(0, color='black')
+    plt.axvline(0, color='black')
+
+    plt.plot(X, f(X))
+    plt.scatter(points_x, points_y)
+    plt.show()
+
+integral_montecarlo(np.sin, 0, np.pi)
+
+print("Square using definite integral:",def_integral(np.sin,0, np.pi))
+
+#get_square(f,1 , 2)
